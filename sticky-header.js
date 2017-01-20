93557f117cdc39688b2ec1680a2d65ff27dd7e6c
Aleksey Razbakov <ar@kirchbergerknorr.de>
2017-01-20 15:46:08 +0100

    add stickyOnHover configuration
    - triggers hoverClass on hover of the $element
    - $element remains sticky if stickyOnHover true

--- sticky-header.js
+++ sticky-header.js
@@ -12,7 +12,9 @@ var jQuery = require('jquery');
             minTop: 0,
             isStickyClass: 'is-sticky',
             stickyWrapperClass: 'sticky-wrapper',
-            scrollTopClass: 'scroll-top'
+            scrollTopClass: 'scroll-top',
+            hoverClass: 'sticky-hover',
+            stickyOnHover: true
         }, options );
 
         return this.each(function() {
@@ -26,61 +28,81 @@ var jQuery = require('jquery');
             var $body = $('body');
 
             var targetElement = settings.targetElement || this;
-            
+
+            var minTop = settings.minTop || $(targetElement).offset().top;
+
             var stickyElementHeight = $(targetElement).height()
                 + parseInt($(targetElement).css('margin-bottom').replace('px', ''))
                 + parseInt($(targetElement).css('margin-top').replace('px', ''));
 
-            var minTop = settings.minTop || $(targetElement).offset().top;
+            function update() {
+                if (settings.minimalViewportWidth > 0 && verge.viewportW() > settings.minimalViewportWidth) {
+                    return false;
+                }
 
-            $(window).scroll(function (event) {
-                if (settings.minimalViewportWidth == 0 || verge.viewportW() < settings.minimalViewportWidth) {
-                    var scrollbarVerticalPosition = $body.scrollTop();
+                var scrollbarVerticalPosition = $body.scrollTop();
 
-                    var isSticky = false;
-                    var isScrollingDown = scrollbarVerticalPosition > scrollbarPreviousVerticalPosition;
-                    var isVisible = scrollbarVerticalPosition <= minTop;
-                    var isVisibleSticky = scrollbarVerticalPosition <= minTop + stickyElementHeight;
+                var isSticky = false;
+                var isScrollingDown = scrollbarVerticalPosition > scrollbarPreviousVerticalPosition;
+                var isVisible = scrollbarVerticalPosition <= minTop;
+                var isVisibleSticky = (settings.type == 'always') || (scrollbarVerticalPosition <= minTop + stickyElementHeight);
+
+                if (settings.stickyOnHover) {
+                    isVisibleSticky = isVisibleSticky || $element.hasClass(settings.hoverClass);
+                }
+
+                if (isVisible) {
+                    isSticky = false;
+                } else if (isScrollingDown) {
+                    isSticky = isVisibleSticky;
+                } else {
+                    isSticky = true;
+                }
+
+                scrollbarPreviousVerticalPosition = scrollbarVerticalPosition;
+
+                if (isCurrentlySticky == isSticky && isCurrentlyVisible == isVisible) {
+                    return;
+                }
+
+                if (intervalId) {
+                    clearTimeout(intervalId);
+                }
+
+                intervalId = setTimeout(function () {
+                    if (isSticky) {
+                        $element.addClass(settings.isStickyClass);
+                    } else {
+                        $element.removeClass(settings.isStickyClass);
+                    }
 
                     if (isVisible) {
-                        isSticky = false;
-                    } else if (isScrollingDown) {
-                        isSticky = (settings.type == 'always') || isVisibleSticky;
+                        $body.addClass(settings.scrollTopClass);
+                        $body.css('padding-top', 0);
+                        $element.removeClass(settings.stickyWrapperClass);
                     } else {
-                        isSticky = true;
+                        $body.removeClass(settings.scrollTopClass);
+                        $body.css('padding-top', stickyElementHeight);
+                        $element.addClass(settings.stickyWrapperClass);
                     }
+                }, settings.timeout);
 
-                    scrollbarPreviousVerticalPosition = scrollbarVerticalPosition;
+                isCurrentlySticky = isSticky;
+                isCurrentlyVisible = isVisible;
+            }
 
-                    if (isCurrentlySticky == isSticky && isCurrentlyVisible == isVisible) {
-                        return;
-                    }
+            if (settings.stickyOnHover) {
+                $element.hover(function () {
+                    $element.addClass(settings.hoverClass);
+                }, function () {
+                    console.log('unhover');
+                    $element.removeClass(settings.hoverClass);
+                    update();
+                })
+            }
 
-                    if (intervalId) {
-                        clearTimeout(intervalId);
-                    }
-
-                    intervalId = setTimeout(function () {
-                        if (isSticky) {
-                            $element.addClass(settings.isStickyClass);
-                        } else {
-                            $element.removeClass(settings.isStickyClass);
-                        }
-
-                        if (isVisible) {
-                            $body.addClass(settings.scrollTopClass);
-                            $body.css('padding-top', 0);
-                            $element.removeClass(settings.stickyWrapperClass);
-                        } else {
-                            $body.removeClass(settings.scrollTopClass);
-                            $body.css('padding-top', stickyElementHeight);
-                            $element.addClass(settings.stickyWrapperClass);
-                        }
-                    }, settings.timeout);
-
-                    isCurrentlySticky = isSticky;
-                    isCurrentlyVisible = isVisible;
-                }
+            $(window).scroll(function (event) {
+                update();
             });
         });
     };
